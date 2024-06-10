661919fb6a1174a98d389b5e16f6d8f7f41a1d11
Sarang S <email.sarangs@gmail.com>
2024-06-11 02:23:17 +0530

    run asyncfl script

---
+++ scripts/run_asyncfl.sh
@@ -0,0 +1,3 @@
+python nectar/sim-async.py --num_cpus 2 --num_gpus 1 --desc "async fl test" --critical_value 0.25 --mi_type "mi_cat" --num_clients 10 --num_rounds 100
+python nectar/sim-async.py --num_cpus 2 --num_gpus 1 --desc "async fl test" --critical_value 0.25 --mi_type "mi_cat" --num_clients 10 --num_rounds 100
+python nectar/sim-async.py --num_cpus 2 --num_gpus 1 --desc "async fl test" --critical_value 0.25 --mi_type "mi_cat" --num_clients 10 --num_rounds 100
