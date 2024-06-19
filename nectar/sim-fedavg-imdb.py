fb282666e99e024f590a091dfbc63561a51cea3b
Sarang S <email.sarangs@gmail.com>
2024-06-19 16:54:33 +0530

    debug

--- nectar/sim-fedavg-imdb.py
+++ nectar/sim-fedavg-imdb.py
@@ -143,11 +143,12 @@ def train(student, trainloader, optim, epochs, device):
         for batch in tqdm(trainloader):
             batch = {k: v.to(DEVICE) for k, v in batch.items()}
             student_outputs = student(**batch)
+            student_logits = student_outputs.logits
             with torch.no_grad():
                 teacher_outputs = teacher(**batch)
-            student_logits = student_outputs.logits
-            teacher_logits = teacher_outputs.logits
-
+                teacher_logits = teacher_outputs.logits
+            print("DEBUG", student_logits, teacher_logits)
+            print("DEBUG", teacher_logits.shape, type(teacher_logits))
             with torch.no_grad():
                 mi_gauss += (
                     mutual_information(
@@ -168,6 +169,7 @@ def train(student, trainloader, optim, epochs, device):
             dist_loss = distiller(student_logits, teacher_logits, batch["labels"])
             # dist_loss = distiller(student_logits, teacher_logits)
             print(f"CE Loss: {ce_loss.item()}, Distill Loss: {dist_loss.item()}")
+            print("DEBUG", 1000 * dist_loss.item())
             loss = ce_loss + dist_loss
             # loss = criterion(student_logits, labels) + distiller(
             #     student_logits, teacher_logits, labels
